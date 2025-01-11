5e941450b0b601a7af9ecdcdf53f6d51e62d4423
rxmlp <62749392+rxmlp@users.noreply.github.com>
2025-01-11 17:54:34 +0100

    some terminal

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,11 +13,6 @@ if status is-interactive
 end
 
 starship init fish | source
-if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
-    cat ~/.cache/ags/user/generated/terminal/sequences.txt
-end
-
-alias pamcan=pacman
 
 alias ..="cd .. "
 
