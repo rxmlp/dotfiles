070e0e9a79858611f4371e0fd62fa87225d860de
rxmlp <62749392+rxmlp@users.noreply.github.com>
2025-08-29 06:07:31 +0200

    more

--- config/hypr/hyprland/scripts/wall/env.sh
+++ config/hypr/hyprland/scripts/wall/env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash 
 
-config_file="$HOME/.config/hypr/hyprland/devices.conf"
+devices="$HOME/.config/hypr/hyprland/conf/devices.conf"
 
 monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .description')
 monitors=$(hyprctl monitors -j | jq -r '.[] | .description')
-monitor_primary_desc=$(grep '^\$monitor_primary = desc:' "$config_file" | sed -E 's/^\$monitor_primary = desc:(.*)$/\1/')
+monitor_primary_desc=$(grep '^\$monitor_primary = desc:' "$devices" | sed -E 's/^\$monitor_primary = desc:(.*)$/\1/')
 monitor_primary="$monitor_primary_desc"
 monitor_primary_port=$(hyprctl monitors -j | jq -r --arg desc "$monitor_primary" '.[] | select(.description == $desc) | .name')
 monitor_primary_path=monitor_primary
-monitor_secondary_desc=$(grep '^\$monitor_secondary = desc:' "$config_file" | sed -E 's/^\$monitor_secondary = desc:(.*)$/\1/')
+monitor_secondary_desc=$(grep '^\$monitor_secondary = desc:' "$devices" | sed -E 's/^\$monitor_secondary = desc:(.*)$/\1/')
 monitor_secondary="$monitor_secondary_desc"
 monitor_secondary_port=$(hyprctl monitors -j | jq -r --arg desc "$monitor_secondary" '.[] | select(.description == $desc) | .name')
 monitor_secondary_path=monitor_secondary
