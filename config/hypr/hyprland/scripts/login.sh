ff6eb21029fa74e8f5904d847092d3b74b8dce36
rxmlp <62749392+rxmlp@users.noreply.github.com>
2025-10-13 23:53:39 +0200

    1

--- config/hypr/hyprland/scripts/login.sh
+++ config/hypr/hyprland/scripts/login.sh
@@ -3,4 +3,4 @@ hyprlock
 $HOME/.config/hypr/hyprland/scripts/wall/wallpaper-restore.sh
 hyprctl dispatch workspace 10
 hyprctl dispatch workspace 1
-$HOME/.config/hypr/hyprland/scripts/session-management/hyprland-restore-session
+$HOME/.local/bin/hyprsessionmanager --ask-restore-latest
