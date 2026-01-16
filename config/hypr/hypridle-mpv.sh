c9ffdeaae5a3ecd608a0e3e2ac75597131fda735
rxmlp <62749392+rxmlp@users.noreply.github.com>
2026-01-16 20:59:45 +0100

    hypridle

--- config/hypr/hypridle-mpv.sh
+++ config/hypr/hypridle-mpv.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-source $HOME/.config/hypr/hyprland/scripts/env.sh
+source $HLS/moni-env.sh
 pause() {
-if pgrep -f mpvpaper > /dev/null; then 
-    echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpv-socket-"$monitor_primary_port" > /dev/null 2>&1 
+if pgrep -f mpvpaper > /dev/null; then
+    echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpv-socket-"$monitor_primary_port" > /dev/null 2>&1
 fi
 }
 
 resume() {
-if pgrep -f mpvpaper > /dev/null; then 
-    echo '{ "command": ["set_property", "pause", false] }' | socat - /tmp/mpv-socket-"$monitor_primary_port" > /dev/null 2>&1 
+if pgrep -f mpvpaper > /dev/null; then
+    echo '{ "command": ["set_property", "pause", false] }' | socat - /tmp/mpv-socket-"$monitor_primary_port" > /dev/null 2>&1
 fi
 }
 
